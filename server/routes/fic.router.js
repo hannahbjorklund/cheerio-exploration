16c4403e82f79d5549a9f81d6977698e32d8a1d0
Hannah Bjorklund <bjorklundhannah@gmail.com>
2024-02-06 22:16:27 -0600

    Created a UI and fixed some problems with the fic notes scraping

--- server/routes/fic.router.js
+++ server/routes/fic.router.js
@@ -7,11 +7,12 @@ const cheerio = require('cheerio');
  * Get the fic data for a fic by ID
  */
 router.get('/:id', (req, res) => {
+  console.log("In get route")
   const id = req.params.id;
 
   axios({
     method: 'GET',
-    url: `https://archiveofourown.org/works/${id}?view_full_work=true`
+    url: `https://archiveofourown.org/works/${id}?view_adult=true&view_full_work=true`
   }).then((response) => {
 
     // Scraping using cheerio
@@ -25,7 +26,7 @@ router.get('/:id', (req, res) => {
     const author = $('.byline.heading').text().trim();
     const rating = $('.rating.tags ul').text().trim();
     const language = $('dd.language').text().trim();
-    const series = $('.series span:first').text().trim();
+    const series = $('.position a:first').text().trim();
     const warnings = [];
     $('.warning.tags li').each((i, el) => {
       warnings.push($(el).text().trim());
@@ -54,14 +55,17 @@ router.get('/:id', (req, res) => {
     $('.stats dt').each((i, el) => {
       stats[($(el).text().trim().slice(0, -1))] = $(el).next().text().trim();
     })
+
+    // -----------------------------------------------------------------
+
     const summary = [];
-    $('.summary.module:first p').each((i , el) => {
-      summary.push($(el).text().trim());
-    })
+    // $('.preface.group .summary.module blockquote:first').each((i , el) => {
+    //   summary.push($(el).text().trim());
+    // })
     const notes = [];
-    $('.notes.module:first p').each((i, el) => {
-      notes.push($(el).text().trim());
-    })
+    // $('.preface.group .notes.module blockquote:first').each((i, el) => {
+    //   notes.push($(el).text().trim());
+    // })
 
     // Creating a fic object
     const fic = {
@@ -82,33 +86,45 @@ router.get('/:id', (req, res) => {
       chapters: []
     };
     
-    // For each chapter, 
-    $('div.chapter.preface.group').each((i, el) => {
-      // Scrape the chapter title
-      const chapter_title = $(el).find('.title').text().trim();
+    // For each chapter div
+    $('div#chapters').children().each((i, el) => {
+      // Each chapter div has 2 unique children, preface group and userstuff
+      // Create a chapter object
+      const chapter = {
+        chapter_title: '',
+        chapter_summary: [],
+        chapter_notes_start: [],
+        chapter_notes_end: [],
+        chapter_text: []
+      }
 
-      // Fics on AO3 can also have a notes and summary for each chapter
-      const chapter_notes_start = [];
-      $(el).find('.notes.module:first blockquote').each((j, elem) => {
-        chapter_notes_start.push($(elem).text().trim());
-      })
-      const chapter_notes_end = [];
-      $(el).find('.end.notes.module:first blockquote').each((j, elem) => {
-        chapter_notes_end.push($(elem).text().trim());
-      })
-      const chapter_summary = [];
-      $(el).find('.summary.module:first blockquote').each((j, elem) => {
-        chapter_summary.push($(elem).text().trim());
-      })
-      
-      // Create a new object for each chapter, with two properties
-      fic.chapters.push({chapter_title, chapter_notes_start, chapter_notes_end, chapter_summary, chapter_text: []});
+      $(el).children().each((j, chil) => {
+        // In the first preface group, scrape chapter title, summary, and notes
+        if (j==0){
+          chapter.chapter_title = $(chil).find('.title').text().trim();
+          $(chil).find('.summary.module blockquote').each((k, line) => {
+            chapter.chapter_summary.push($(line).text().trim());
+          })
 
-      // Scrape the chapter lines
-      $(el).next().find('p').each((j, elem) => {
-        fic.chapters[i].chapter_text.push($(elem).text().trim());
-      });
-    });
+          $(chil).find('.notes.module blockquote').each((k, line) => {
+            chapter.chapter_notes_start.push($(line).text().trim());
+          })
+          
+          // In the chapter body, scrape paragraphs
+        } else if(j == 1){
+          $(chil).find('p').each((k, line) => {
+            chapter.chapter_text.push($(line).text().trim());
+          });
+          
+          // If there are more than two children, then there is an end of chapter notes section we need to grab
+        } else {
+          $(chil).find('.end.notes.module blockquote').each((k, line) => {
+            chapter.chapter_notes_end.push($(line).text().trim());
+          });
+        }
+      })
+      fic.chapters.push(chapter);
+    })
 
     // Retrieved all fic data and formatted object. Time to send!
     res.send(fic);
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button } from '@mui/material';
+import axios from 'axios';
+
 import './App.css';
 
 function App() {
+  const [inputURL, setInputURL] = useState('');
+
+  /**
+   * Handles form submission when user clicks submit button. Will strip the fic ID from the given url and
+   *  make an axios request for the fic info
+   * @param {*} e event
+   */
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Trim the id from the provided url, the id will be between works/id/chapters
+    const ficID = inputURL.substring(inputURL.indexOf('works/') + 6, inputURL.indexOf('/chapters'));
+    
+    // Send a request to the server
+    axios({
+      method: 'GET',
+      url: `/fic/${ficID}`
+    }).then((result) => {
+      console.log(result)
+    })
+  }
+
+  const handleClear = (e) => {
+    e.preventDefault();
+    setInputURL('');
+  }
+
   return (
     <div className = 'container'>
-      Test
+      <h1>Import an AO3 work:</h1>
+      <form onSubmit={handleSubmit}>
+        <label>URL: </label>
+        <input
+          placeholder='AO3 url'
+          value={inputURL}
+          onChange = {(e) => setInputURL(e.target.value)}
+        />
+        <Button type='submit' variant='contained'>Submit</Button>
+        <Button variant='outlined' onClick={handleClear}>Clear</Button>
+      </form>
     </div>
   );
 }
