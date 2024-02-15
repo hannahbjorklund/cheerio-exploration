6293b7f33b09ee9d835050246e090bc7b1dd2142
Hannah Bjorklund <bjorklundhannah@gmail.com>
2024-02-14 18:04:10 -0600

    Added an accordion for fic stats, and did formatting for mobile view

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -35,7 +35,7 @@ function App() {
       url: `/api/ao3/work/${ficID}`
     }).then((result) => {
         setFicData(result.data);
-        console.log(ficData);
+        console.log(result.data);
     })
     setInputURL('');
   }
@@ -51,26 +51,36 @@ function App() {
 
   return (
     <div className = 'container'>
-      <h1>Import an AO3 work:</h1>
-      <form onSubmit={handleSubmit}>
-        <label>URL: </label>
-        <input
-          placeholder='AO3 url'
-          value={inputURL}
-          onChange = {(e) => setInputURL(e.target.value)}
-        />
-        <Button type='submit' variant='contained'>Submit</Button>
-        <Button sx={{marginLeft: '0.5em'}} variant='outlined' onClick={handleClear}>Clear</Button>
-      </form>
+      <div className = 'site-header'>
+        <h1>Import an AO3 work:</h1>
+        <form onSubmit={handleSubmit}>
+          <div className = 'input-group'>
+            <label>URL: </label>
+            <input
+              required
+              placeholder='AO3 url'
+              value={inputURL}
+              onChange = {(e) => setInputURL(e.target.value)}
+            />
+          </div>
+          <div className = 'button-group'>
+            <Button size='medium' type='submit' variant='contained'>Submit</Button>
+            <Button sx={{marginLeft: '0.5em'}} variant='outlined' onClick={handleClear}>Clear</Button>
+          </div>
+        </form>
+      </div>
       <hr/>
       <div className = 'pre-text'>
-        <h1>{ficData.title || ""}</h1>
-        {ficData.author && <h2>by {ficData.author}</h2>}
-        {//ficData.stats && 
+        <div className = 'title-group'>
+          <h1 className = 'title'>{ficData.title || ""}</h1>
+          {ficData.author && <h2>by {ficData.author}</h2>}
+        </div>
+        {ficData.stats && 
           <Accordion sx={{fontFamily: 'Open Sans',
           backgroundColor: '#303030',
           color: 'antiquewhite',
-          fontSize: '18px'}}>
+          fontSize: '19px',
+          width: '95%'}}>
             <AccordionSummary
               expandIcon={<ExpandMore style={{color: 'antiquewhite'}}/>}
               aria-controls='panel1-content'
@@ -78,24 +88,40 @@ function App() {
             >
               Work Stats
             </AccordionSummary>
-            <AccordionDetails sx={{backgroundColor: '#343434'}}>
-              Test
+            <AccordionDetails sx={{backgroundColor: '#343434', fontFamily: 'Open Sans', fontSize: '16px'}}>
+              <div className = 'stats-container'>
+                <div>
+                  <p>Words: {ficData.stats.words}</p>
+                  <p>Chapters: {ficData.stats.chapters}</p>
+                  <p>Published: {ficData.stats.published}</p>
+                  <p>Completed: {ficData.stats.completed}</p>
+                </div>
+                <div>
+                  <p>Hits: {ficData.stats.hits}</p>
+                  <p>Kudos: {ficData.stats.kudos}</p>
+                  <p>Comments: {ficData.stats.comments}</p>
+                  <p>Bookmarks: {ficData.stats.bookmarks}</p>
+                </div>
+              </div>
             </AccordionDetails>
           </Accordion>
         }
       </div>
+      <br/>
+      {ficData && <hr/>}
       <div className = 'text-body'>
         {ficData && ficData.chapters.map((x, i) => {
           return (
-            <h3 id={i} className = 'chap-header'>
-              {x.chapter_title}
-              <hr/>
-              {
-                x.chapter_text.map((y, j) => {
-                  return <p id={j} className = 'chap-line'>{y}</p>
-                })
-              }
-            </h3>
+            <>
+              <h2 id={i} className = 'chap-header'>
+                {x.chapter_title}
+              </h2>
+                {
+                  x.chapter_text.map((y, j) => {
+                    return <p id={j} className = 'chap-line'>{y}</p>
+                  })
+                }
+            </>
           )
         })}
       </div>
