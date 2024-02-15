5dff792397464b8771ab50185acd8fc5b63681c7
Hannah Bjorklund <bjorklundhannah@gmail.com>
2024-02-15 10:05:12 -0600

    changed default port

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5002;
 
 // Route Includes
 const ao3Router = require('./routes/ao3.router');
