bff3edea921f574608a80c832898edac16476b7b
Hannah Bjorklund <bjorklundhannah@gmail.com>
2024-02-15 23:27:45 -0600

    hamburger menu with toggling icon

--- src/components/HideAppBar/HideAppBar.jsx
+++ src/components/HideAppBar/HideAppBar.jsx
@@ -1,18 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   AppBar,
   Toolbar,
   Slide,
   useScrollTrigger,
-  Typography
-} from "@mui/material";
-import { ExpandLess } from "@mui/icons-material";
+  Typography,
+  Menu,
+  MenuItem,
+  Button,
+} from "@mui/material"
+import { ExpandLess, Menu as MenuIcon, MenuOpen as MenuOpenIcon} from "@mui/icons-material";
 
 export default function HideAppBar({headerRef}) {
   const trigger = useScrollTrigger();
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
 
   /**
-   * When user clicks the Top icon in the app bar, scroll them to the top of the page
+   * When user clicks the Top icon in the app bar, scroll them to the top of the page 
    */
   const scrollToTop = () => {
     window.scrollTo({
@@ -21,10 +26,16 @@ export default function HideAppBar({headerRef}) {
     })
   }
   
+  const handleMenuClick = (e) => {
+    setMenuIsOpen(!menuIsOpen);
+    setAnchorEl(e.currentTarget);
+  }
+
   return (
     <Slide appear={false} direction="up" in={!trigger}>
-      <AppBar sx={{top: 'auto', bottom: 0, height: '3.5em', display: 'flex', alignItems: 'flex-end', justifyContent: 'center'}}>
+      <AppBar sx={{top: 'auto', bottom: 0, height: '3.5em', display: 'flex', alignItems: 'flex-start'}}>
         <Toolbar>
+          <Button onClick={handleMenuClick}>{menuIsOpen ? <MenuOpenIcon sx={{color: 'white'}}></MenuOpenIcon> : <MenuIcon sx={{color: 'white'}}/>}</Button>
           <Typography onClick = {scrollToTop} component="div">
             Top <ExpandLess sx={{verticalAlign: 'middle'}}/>
           </Typography>
